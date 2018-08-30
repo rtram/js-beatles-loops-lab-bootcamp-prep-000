6411a50aa87650c9d9127be41dc0716e6d8abbfa
Robin Tram <robin.tram@gmail.com>
2018-08-30 17:00:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 function theBeatlesPlay (musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push (`${musicians[i]} plays ${instruments[i]}`);
-  }
-  return array;
+	let array = [];
+	for (let i = 0; i < musicians.length; i++) {
+		array.push(`${musicians[i]} plays ${instruments[i]}`);
+	}
+	return array;
 }
 
 function johnLennonFacts (facts) {
-  var array = [];
-  for (var i = 0; i < facts.length; i++) {
-    array.push (facts[i] = `${facts[i]}!!!`);
-  }
-  return array;
+	let i = 0;
+	let array = [];
+	while (i < facts.length) {
+		array.push(`${facts[i]}!!!`);
+		i++;
+	}
+	return array;
 }
 
 function iLoveTheBeatles (number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number++;
-  } while (number < 15);
-  return array;
+	let array = [];
+	do {
+		array.push(`I love the Beatles!`);
+		number++;
+	}	while (number < 15); 
+	return array;
 }
